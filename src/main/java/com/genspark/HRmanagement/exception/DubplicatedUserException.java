8a16746a618e128a64d5ee00e8d475c044a64bf8
hana <hanane7885@gmail.com>
2023-03-28 09:54:37 -0500

    Update DubplicatedUserException.java

--- src/main/java/com/genspark/HRmanagement/exception/DubplicatedUserException.java
+++ src/main/java/com/genspark/HRmanagement/exception/DubplicatedUserException.java
@@ -1,8 +1,8 @@
-package com.genspark.HRmanagement.exception;
-
-public class DubplicatedUserException extends RuntimeException{
-	
-	public DubplicatedUserException(String s){
-		super(s);
-	}
-}
+//package com.genspark.HRmanagement.exception;
+//
+//public class DubplicatedUserException extends RuntimeException{
+//
+//	public DubplicatedUserException(String s){
+//		super(s);
+//	}
+//}
